ae37cd18ef5a75024cade0e7c5f885abb2e7591e
sebbex1337 <sefort@hotmail.dk>
2024-05-17 09:44:51 +0200

    first commit

---
+++ src/main/java/exam/kommunalvalg/KommunalvalgApplication.java
@@ -0,0 +1,13 @@
+package exam.kommunalvalg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommunalvalgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KommunalvalgApplication.class, args);
+    }
+
+}
